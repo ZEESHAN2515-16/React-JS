29740b2f6c0aa284dcc53ac24b84b757d9a538d0
Zeeshan Nasir <zeeshannasir2515@gmail.com>
2021-05-16 13:17:01 +0500

    App Started

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Palette from "./Palette";
+import seedColors from "./seedColors";
+function App() {
+	return (
+		<div className="App">
+			<Palette />
+		</div>
+	);
+}
+
+export default App;
---
+++ src/Palette.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Palette extends Component {
+	render() {
+		return (
+			<div>
+				<h2>Palette</h2>
+			</div>
+		);
+	}
+}
+
+export default Palette;
