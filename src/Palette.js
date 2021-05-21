c27fb593dc3c7ab4784bdce5ec41b9b6a57fbec2
Zeeshan Nasir <zeeshannasir2515@gmail.com>
2021-05-21 17:43:16 +0500

    Small Change

--- src/Palette.js
+++ src/Palette.js
@@ -3,14 +3,13 @@ import ColorBox from './ColorBox';
 import './Palette.css';
 class Palette extends Component {
    render() {
-      const colorBoxes = this.props.colors.map((color) => (
-         <ColorBox background={color.color} name={color.name} />
-      ));
+      const colorBoxes = this.props.colors.map((color) => <ColorBox background={color.color} name={color.name} />);
       return (
          <div className='Palette'>
             {/* navber goes here */}
             <div className='Palette-colors'>{colorBoxes}</div>
             {/* footer goes here */}
+            {/* Something also goes here */}
          </div>
       );
    }
